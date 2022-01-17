af1e3539dd90fad76697d02a062d200c9ec8bed2
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-17 14:38:04 +0200

    Done

--- P9-05/ContentView.swift
+++ P9-05/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .frame(width: 150, height: 150)
+                .background(.red)
+            
+            Text("Hello, world!")
+                .frame(width: 150, height: 150)
+                .border(.red, width: 30)
+            
+            Text("Hello World")
+                .frame(width: 150, height: 150)
+                .border(ImagePaint(image: Image("california"), scale: 0.2), width: 30)
+            
+            Text("Hello World")
+                .frame(width: 150, height: 150)
+                .border(ImagePaint(image: Image("california"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+            
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("california"), scale: 0.1), lineWidth: 20)
+                .frame(width: 150, height: 150)
+        }
     }
 }
 
